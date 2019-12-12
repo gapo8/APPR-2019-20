2a883197c4d532f225e49259510190bb0ecbec97
Gašper Potočnik <gasper1999@gmail.com>
2019-12-12 09:38:20 +0100

    uvozil podatke

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,7 @@ library(rvest)
 library(gsubfn)
 library(tidyr)
 library(shiny)
+library(httr)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
